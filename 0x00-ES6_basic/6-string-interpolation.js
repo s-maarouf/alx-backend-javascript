f82e196f61dfeed1020c38e2dc73ba80d8221b55
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-04 22:07:09 +0100

    Completeed task 6

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,10 @@
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
+
+  return `As of ${year}, it was the highest-income county in the United States, with a per capita personal income of $${budget.capita}. It was the fifth-most affluent county in the United States by per capita income, and has the highest concentration of million-dollar homes in the United States.`;
+}
