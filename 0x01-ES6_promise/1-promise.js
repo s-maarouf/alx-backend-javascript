cdfe51671daedbba6d9be8cb1e58490d99048c37
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-05 20:46:52 +0100

    Fixed typo errors

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,10 +3,10 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve({
         status: 200,
-        body: 'success',
+        body: 'Success',
       });
     } else {
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
