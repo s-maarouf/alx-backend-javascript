556f5d821e098e44195113d51230c88e0e68d26a
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-06 23:35:03 +0100

    Reimplementation

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const pic = await uploadPhoto();
-  const user = await createUser();
   let res = {};
 
   try {
+    const pic = await uploadPhoto();
+    const user = await createUser();
     res = { pic, user };
   } catch (err) {
     res = { pic: null, user: null };
