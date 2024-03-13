a6b8539563a831f938361a345cace5e6f99e2402
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-13 02:11:49 +0000

    Completed task 3

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,6 @@
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce((prev, curr) => prev.id || prev + curr.id, 0);
+  }
+  return 0;
+}
