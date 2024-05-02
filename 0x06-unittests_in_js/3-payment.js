fd09ca45316c0a7d135f3ae3299af7ce0e3c7cf7
s-maarouf <maarouf.salahedd@gmail.com>
2024-05-02 22:59:39 +0100

    Completed task 3

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,7 @@
+import Utils from "./utils";
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    console.log('The total is: ${}', Utils.calculateNumber('SUM', totalAmount, totalShipping));
+}
+
+module.exports = sendPaymentRequestToApi;
