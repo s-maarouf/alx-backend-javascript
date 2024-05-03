03e6faf829b6b96de4bea2f15dc9a6eba3fa48de
s-maarouf <maarouf.salahedd@gmail.com>
2024-05-03 17:19:30 +0100

    Fixed requirement output

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,7 +1,7 @@
-import Utils from "./utils";
+const Utils = require('./utils')
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    console.log('The total is:', Utils.calculateNumber('SUM', totalAmount, totalShipping));
+    console.log(`The total is: ${Utils.calculateNumber('SUM', totalAmount, totalShipping)}`);
 }
 
 module.exports = sendPaymentRequestToApi;
